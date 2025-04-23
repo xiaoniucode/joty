b3889d583803edb8cfbff24740fed84cb8a28b23
xilio <xilio1024@gmail.com>
2025-04-23 13:57:09 +0800

    feat:  短链接接口定义

--- src/main/java/cn/xilio/leopard/api/portal/controller/PortalShortUrlController.java
+++ src/main/java/cn/xilio/leopard/api/portal/controller/PortalShortUrlController.java
@@ -1,13 +1,36 @@
 package cn.xilio.leopard.api.portal.controller;
 
 import cn.xilio.leopard.common.util.Result;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.xilio.leopard.domain.shorturl.service.ShortUrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/short-url")
 public class PortalShortUrlController {
+    @Autowired
+    private ShortUrlService shortUrlService;
 
+    @PostMapping(value = "create", name = "创建单个")
+    public Result createSingleShortUrl() {
+
+        return Result.success();
+    }
+
+    @PostMapping(value = "create-batch", name = "批量创建")
+    public Result createBatchShortUrl() {
+
+        return Result.success();
+    }
+
+    @GetMapping(value = "list", name = "")
+    public Result getShortUrls() {
+        return Result.success();
+    }
+
+    @DeleteMapping(value = "del", name = "")
+    public Result deleteShortUrls() {
+        return Result.success();
+    }
 
 }
