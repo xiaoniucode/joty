3f2c2bf6c6a5984e860d5ff5377c1dc875079c28
xilio <xilio1024@gmail.com>
2025-04-29 19:32:01 +0800

    feat: 完善API swagger接口注释

--- leopard-server/biz/src/main/java/cn/xilio/leopard/biz/adapter/portal/controller/PortalShortUrlController.java
+++ leopard-server/biz/src/main/java/cn/xilio/leopard/biz/adapter/portal/controller/PortalShortUrlController.java
@@ -6,6 +6,7 @@ import cn.xilio.leopard.biz.adapter.portal.dto.request.CreateSingleShortUrlReque
 import cn.xilio.leopard.biz.domain.shorturl.service.ShortUrlService;
 import cn.xilio.leopard.core.common.page.PageQuery;
 import cn.xilio.leopard.core.common.util.Result;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -15,32 +16,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/short-url")
-@Tag(name = "Short Link")
+@Tag(name = "短链接口")
 public class PortalShortUrlController {
     @Autowired
     private ShortUrlService shortUrlService;
 
+    @Operation(summary = "创建单个短链接")
     @PostMapping(value = "create", name = "Create a single")
     public Result createSingleShortUrl(@Validated @RequestBody CreateSingleShortUrlRequest request) {
         return Result.success(shortUrlService.createSingle(request));
     }
 
+    @Operation(summary = "批量创建短链接")
     @PostMapping(value = "create-batch", name = "Batch creation")
     public Result createBatchShortUrl(@Validated @RequestBody CreateBatchShortUrlRequest request) {
         return Result.success(shortUrlService.createBatchShortUrl(request));
     }
 
+    @Operation(summary = "短链接列表")
     @PostMapping(value = "list", name = "Short link pagination list")
     public Result getShortUrls(@Validated @RequestBody PageQuery request) {
         return Result.success(shortUrlService.getShortUrls(request));
     }
 
+    @Operation(summary = "批量删除短链接")
     @DeleteMapping(value = "del", name = "Batch delete short links")
     public Result deleteShortUrls(@RequestBody List<String> ids) {
         shortUrlService.deleteShortUrl(ids);
         return Result.success();
     }
 
+    @Operation(summary = "获取短链接信息")
     @GetMapping(value = "get", name = "Get short link information")
     public Result getShortUrlInfo(@RequestParam String id) {
         return Result.success(shortUrlService.getShortUrlInfo(id));
