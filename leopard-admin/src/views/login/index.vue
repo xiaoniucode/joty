a88b718cc52aca615dad4433f2f982dbacf411aa
xilio <xilio1024@gmail.com>
2025-05-06 22:29:47 +0800

    feat: 引入请求用户设备类型解析

--- leopard-admin/src/views/login/index.vue
+++ leopard-admin/src/views/login/index.vue
@@ -1,37 +1,47 @@
 <template>
-  <div class="flex justify-center px-[5%] pt-[10%]">
-    <a-form
-      :model="formState"
-      name="basic"
-      autocomplete="off"
-      @finish="onFinish"
-      @finishFailed="onFinishFailed"
-    >
-      <a-form-item
-        label="用户名"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
-      >
-        <a-input v-model:value="formState.username" />
-      </a-form-item>
+  <a-row>
+    <a-col :span="12"  class="login-bg">
 
-      <a-form-item
-        label="密码"
-        name="password"
-        :rules="[{ required: true, message: 'Please input your password!' }]"
-      >
-        <a-input-password v-model:value="formState.password" />
-      </a-form-item>
 
-      <a-form-item name="remember">
-        <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
-      </a-form-item>
+    </a-col>
+    <a-col :span="12">
+      <div class="flex justify-center px-[5%] pt-[10%]">
+        <a-form
+            :model="formState"
+            name="basic"
+            autocomplete="off"
+            @finish="onFinish"
+            @finishFailed="onFinishFailed"
+        >
+          <a-form-item
+              label="用户名"
+              name="username"
+              :rules="[{ required: true, message: 'Please input your username!' }]"
+          >
+            <a-input v-model:value="formState.username" />
+          </a-form-item>
 
-      <a-form-item>
-        <a-button type="primary" html-type="submit">登陆</a-button>
-      </a-form-item>
-    </a-form>
-  </div>
+          <a-form-item
+              label="密码"
+              name="password"
+              :rules="[{ required: true, message: 'Please input your password!' }]"
+          >
+            <a-input-password v-model:value="formState.password" />
+          </a-form-item>
+
+          <a-form-item name="remember">
+            <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
+          </a-form-item>
+
+          <a-form-item>
+            <a-button type="primary" html-type="submit">登陆</a-button>
+          </a-form-item>
+        </a-form>
+      </div>
+
+    </a-col>
+  </a-row>
+
 </template>
 <script lang="ts" setup>
 import { reactive } from 'vue'
@@ -59,4 +69,9 @@ const onFinishFailed = (errorInfo: any) => {}
 
 <style scoped>
  @import "tailwindcss";
+ .login-bg{
+   background-image: url("/login_bg.jpg");
+   object-fit: contain;
+   height: 100vh;
+ }
 </style>
